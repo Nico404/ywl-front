e935e3c89f893f7e0496e7c0ee769bbcfa6752d2
nicolas geron <nicolas.geron@gmail.com>
2023-06-15 15:25:51 +0200

    much bigger

--- app.py
+++ app.py
@@ -33,6 +33,7 @@ def main():
 def display_predictions(predictions):
     # Display avatars and top 3 predictions with probabilities
     st.write("Top 3 Predictions:")
+    avatar_size = 300
     for i, (author, prob) in enumerate(predictions.items()):
         if i >= 3:
             break
@@ -40,7 +41,7 @@ def display_predictions(predictions):
         # Display avatar
         image_path = f"assets/avatar_{author}.jpg"
         avatar_caption = f"#{i + 1} - {author}"
-        st.image(image_path, width=150, caption=avatar_caption)
+        st.image(image_path, width=avatar_size, caption=avatar_caption, use_column_width=True, output_format="auto")
 
 if __name__ == "__main__":
     main()
