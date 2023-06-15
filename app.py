4950964c12cc4251a3859b98aafb136cc6191ffe
nicolas geron <nicolas.geron@gmail.com>
2023-06-15 14:52:06 +0200

    new predictions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests
-
+import streamlit as st
+import requests
 
 def main():
     # Streamlit app title
@@ -8,7 +9,7 @@ def main():
 
     # Display the description
     st.markdown("""
-    YouWriteLike is an AI Bot that will match your writing style with some of the most influential authors out there.
+    YouWriteLike is an application that will match your writing style with some of the most influential authors out there.
     We have built classification models that can predict which writer's style is closest to your own writing style.
     Now, play, write, and learn!
     """)
@@ -27,10 +28,22 @@ def main():
         if response.status_code == 200:
             result = response.json()
             st.success("Prediction Result:")
-            st.write(result)
+            display_predictions(result)
         else:
             st.error("Error occurred during prediction.")
 
+def display_predictions(predictions):
+    # Display avatars and top 3 predictions with probabilities
+    st.write("Top 3 Predictions:")
+    for i, (author, prob) in enumerate(predictions.items()):
+        if i >= 3:
+            break
+
+        # Display avatar
+        st.image(f"assets/avatar_{author.lower()}.png", width=50, caption=author)
+
+        # Display probability
+        st.write(f"Probability: {prob}")
 
 if __name__ == "__main__":
     main()
