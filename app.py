fa428a1c9ea2086bb9e1a1028055f7470611924d
nicolas geron <nicolas.geron@gmail.com>
2023-06-16 11:54:26 +0200

    test size

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def display_predictions(predictions):
         # Display avatar
         image_path = f"assets/avatar_{author}.jpg"
         avatar_caption = f"#{i + 1} - {author}"
-        col1, col2, col3 = st.columns([0.2, 5, 0.2])
+        col1, col2, col3 = st.columns([1, 2, 1])
         col2.image(image_path, width=avatar_size, caption=avatar_caption, use_column_width=True, output_format="auto")
 
 if __name__ == "__main__":
